db892a5806e4f84db68cc7c445b255527466ab2b
MuhammadRafik <aeranaan@gmail.com>
2025-05-06 09:31:36 +0800

    Add files via upload

--- UTS.php
+++ UTS.php
@@ -1,6 +1,6 @@
 <?php
-if (isset($_GET['nama'])) {
-    $nama = htmlspecialchars($_GET['nama']);
+if (isset($_GET['Muhammad Rafik'])) {
+    $nama = htmlspecialchars($_GET['Muhammad Rafik']);
     echo "Halo, $nama!";
 } else {
     echo "Nama tidak ditemukan di URL.";
